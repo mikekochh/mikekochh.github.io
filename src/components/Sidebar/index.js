c949ccd2512bd4045bf0d49b6567c43c3015ff33
mikekochh <michaelkoch4444@gmail.com>
2023-09-23 21:49:23 -0400

    adding logo highlighting

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,30 @@
 import './index.scss'
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import mkLogo from '../../assets/images/mkLogo.png';
+import blackLogo from '../../assets/images/blackLogo.jpg';
+import highlightedLogo from '../../assets/images/highlightedLogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser, faX } from '@fortawesome/free-solid-svg-icons';
 import { faGit, faGithub, faLinkedin, faTwitter, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 
-const Sidebar = () => (
+const Sidebar = () => {
+
+    const [logo, setLogo] = useState(blackLogo);
+
+    const highlightLogo = () => {
+        setLogo(highlightedLogo);
+    }
+
+    const unhighlightLogo = () => {
+        setLogo(blackLogo);
+    }
+
+
+
+return (
     <div className='nav-bar'>
         <Link to='/'>
-            <img className='logo' src={mkLogo} alt='Logo' />
+            <img className='logo' src={logo} alt='Logo' onMouseEnter={highlightLogo} onMouseLeave={unhighlightLogo} />
         </Link>
         <nav>
             <NavLink exact="true" activeClassName="active" className="NavLinkHome" to='/'>
@@ -22,7 +38,7 @@ const Sidebar = () => (
             </NavLink>
         </nav>
         <ul>
-            <li>
+            <li className='linkedinLink'>
                 <a href='https://www.linkedin.com/in/michael-koch-6378831a2/' target='_blank' rel="noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"  />
                 </a>
@@ -40,5 +56,7 @@ const Sidebar = () => (
         </ul>
     </div>
 )
+
+}
 
 export default Sidebar
